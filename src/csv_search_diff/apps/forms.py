3959b2a72368834d1ee1e7ec4e42004fce589cc4
yui728 <yui.nishimra.1984@gmail.com>
2019-10-23 17:39:52 +0900

    ・CSV設定用のフォームクラスを作成 ・テスト用にshift_jis形式のCSVファイルを追加

--- src/csv_search_diff/apps/forms.py
+++ src/csv_search_diff/apps/forms.py
@@ -4,4 +4,36 @@ from django import forms
 class CsvInputForm(forms.Form):
     csv1 = forms.fields.FileField(
         label='CSV1',
+        required=True,
+        widget=forms.widgets.FileInput
     )
+
+    csv1_no_header = forms.fields.BooleanField(
+        label='ヘッダーなし',
+        required=True,
+        widget=forms.widgets.CheckboxInput
+    )
+
+    csv2 = forms.fields.FileField(
+        lavel='CSV2',
+        required=True,
+        widget=forms.widgets.FileInput
+    )
+
+    csv2_no_header = forms.fields.BooleanField(
+        label='ヘッダーなし',
+        required=True,
+        widget=forms.widgets.CheckboxInput
+    )
+#
+# class DiffColumnSettingForm(forms.Form):
+#
+#
+# class KeyColumnSettingForm(forms.Form):
+#
+#
+# class ConfirmForm(forms.Form):
+#
+#
+# class ResultForm(forms.Form):
+
