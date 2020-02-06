e84d89f611f6e7e0f76544411918c25170f3bcd2
yui728 <yui.nishimra.1984@gmail.com>
2020-02-06 17:23:32 +0900

    ・比較項目の詳細を生成するメソッドを実装 ・キー項目の詳細を生成するメソッドを実装

--- src/csv_search_diff/apps/utility.py
+++ src/csv_search_diff/apps/utility.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 class CalcColumnCountUtility:
     @classmethod
     def get_max_diff_column_count(cls, csv1: pd.DataFrame, csv2: pd.DataFrame):
@@ -9,3 +10,21 @@ class CalcColumnCountUtility:
     def get_max_key_column_count(cls, csv1: pd.DataFrame, csv2: pd.DataFrame, diff_column_count: int):
         max_column_count = (len(csv1.columns)) if len(csv1.columns) <= len(csv2.columns) else (len(csv2.columns))
         return max_column_count - diff_column_count
+
+
+class ColumnDetailMessageUtility:
+    @classmethod
+    def create_diff_columns_detail(cls, csv1_cols: list, csv2_cols: list) -> list:
+        result = []
+        max_loop = len(csv1_cols) if len(csv1_cols) <= len(csv2_cols) else len(csv2_cols)
+        for i in range(max_loop):
+            result.append('CSV1の{}とCSV2の{}を比較する'.format(csv1_cols[i], csv2_cols[i]))
+        return result
+
+    @classmethod
+    def create_key_columns_detail(cls, csv1_cols: list, csv2_cols: list) -> list:
+        result = []
+        max_loop = len(csv1_cols) if len(csv1_cols) <= len(csv2_cols) else len(csv2_cols)
+        for i in range(max_loop):
+            result.append('CSV1の{}とCSV2の{}をキーにする'.format(csv1_cols[i], csv2_cols[i]))
+        return result
--- src/csv_search_diff/tests/apps/test_utility.py
+++ src/csv_search_diff/tests/apps/test_utility.py
@@ -95,5 +95,42 @@ class CalcColumnCountUtilityTest(SimpleTestCase):
         pass
 
 
+class ColumnDetailMessageTestCase(SimpleTestCase):
+    def test_create_diff_columns_detail_01(self) -> None:
+        """比較項目詳細の生成：列名が半角英数字の場合"""
+        csv1_cols = ['col1', 'col2']
+        csv2_cols = ['col3', 'col4']
+        result = utility.ColumnDetailMessageUtility.create_diff_columns_detail(csv1_cols, csv2_cols)
+        self.assertEquals(2, len(result))
+        self.assertEquals('CSV1のcol1とCSV2のcol3を比較する', result[0])
+        self.assertEquals('CSV1のcol2とCSV2のcol4を比較する', result[1])
 
+    def test_create_diff_columns_detail_02(self) -> None:
+        """比較項目詳細の生成：列名が全角文字の場合"""
+        csv1_cols = ['列1', '列2', '列3']
+        csv2_cols = ['列3', '列4', '列5']
+        result = utility.ColumnDetailMessageUtility.create_diff_columns_detail(csv1_cols, csv2_cols)
+        self.assertEquals(3, len(result))
+        self.assertEquals('CSV1の列1とCSV2の列3を比較する', result[0])
+        self.assertEquals('CSV1の列2とCSV2の列4を比較する', result[1])
+        self.assertEquals('CSV1の列3とCSV2の列5を比較する', result[2])
+
+    def test_create_key_columns_detail_01(self) -> None:
+        """キー項目詳細の生成：列名が半角英数字の場合"""
+        csv1_cols = ['col1', 'col2']
+        csv2_cols = ['col3', 'col4']
+        result = utility.ColumnDetailMessageUtility.create_key_columns_detail(csv1_cols, csv2_cols)
+        self.assertEquals(2, len(result))
+        self.assertEquals('CSV1のcol1とCSV2のcol3をキーにする', result[0])
+        self.assertEquals('CSV1のcol2とCSV2のcol4をキーにする', result[1])
+
+    def test_create_key_columns_detail_02(self) -> None:
+        """キー項目詳細の生成：列名が全角文字の場合"""
+        csv1_cols = ['列1', '列2', '列3']
+        csv2_cols = ['列3', '列4', '列5']
+        result = utility.ColumnDetailMessageUtility.create_key_columns_detail(csv1_cols, csv2_cols)
+        self.assertEquals(3, len(result))
+        self.assertEquals('CSV1の列1とCSV2の列3をキーにする', result[0])
+        self.assertEquals('CSV1の列2とCSV2の列4をキーにする', result[1])
+        self.assertEquals('CSV1の列3とCSV2の列5をキーにする', result[2])
 
